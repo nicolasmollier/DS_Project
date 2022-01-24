64ca608257c45a499efda283552879f6936b5f12
nicolasmollier <nicolas.mollier@gmx.de>
2022-01-24 17:38:02 +0100

    html and java script code for functionality and optics in the app

---
+++ scripts/js_html_for_app.R
@@ -0,0 +1,30 @@
+
+# Java Script and HTML for the Shiny App
+
+js <- "
+$(document).ready(function(){
+  $('img').on('click', function(){
+    Shiny.setInputValue('last_click', this.id);
+  });
+})
+"
+
+image_style_gallery <- "cursor:pointer; width:200pt; height:250pt;border: 1px solid #ddd;
+  border-radius: 0px;
+  padding: 8px;
+  width: 200pt;
+  hover:box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);"
+
+image_style_recommendation_shoe <- "cursor:pointer; width:200pt; height:250pt;border: 1px solid #ddd;
+  border-radius: 0px;
+  padding: 8px;
+  width: 200pt;
+  hover:box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);"
+
+image_style_shoe_selected_recommendation_tab <- "cursor:pointer; width:200pt; height:250pt;border: 10px solid #4F4F4F;
+  border-radius: 3px;
+  padding: 10px;
+  width: 200pt;
+  hover:box-shadow: 0 0 2px 1px rgba(0, 0, 0, 0);"
+
+
